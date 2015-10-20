b5ab8b9b48605efcfc1970243d7aa809deaa1184
Christoffer Johansson <christoffer@coldbluegames.com>
2015-10-21 01:41:29 +0200

    Master creation

---
+++ README.md
@@ -0,0 +1,16 @@
+# youJQtube
+
+The youJQtube project is a school assignment with the intention to learn some git, how to use github
+and how to use packagist and composer to create our own packages for inclusion in "frameworks".
+
+I named it youJQtube as it involves jQuery and youtube.
+The name might be a bit misleading. :P
+
+The intention of this particular repo (and it's packagist package) is to provide the ability to create
+div's on the webpage that contains a iframe with the Youtube media player implemented.
+
+The two main features of this is it's jQuery part. Which enables you to resize the 
+div and in essence the Youtube player in it. And to move the div around on the webpage.
+
+***Methods and detailed description of how to use them***
+
---
+++ src/youJQtube/youJQtube.php
@@ -0,0 +1,196 @@
+<?php
+
+namespace fynrir\youJQtube;
+
+/**
+ * A utility class for creating resizeable Youtube mediaplayers for DIVs using javascript and php.
+ * You can also just make it create a normal unmoveable div.
+ * You need to add jQuery and jQueryUI to your head section like so:
+ *
+ * <script type="text/javascript"
+ * src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
+ * <script type="text/javascript"
+ * src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.7.2/jquery-ui.js"></script>
+ * <link rel="stylesheet" type="text/css"
+ * href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.7.1/themes/base/jquery-ui.css"/>
+ * 
+ *
+ * @package youJQtube
+ */
+
+class youJQtube implements \ArrayAccess
+{
+
+	/**
+	 * Properties
+	 */
+
+	$youtubeurlID_; //The extracted ID from the youtube url.
+	$options_; 		//The options.
+	$origin_;       //It's extremly important that you change the MYDOMAIN text to your full domain name and page where the player is to appear on.
+                    //If you use a frontcontroller. It should be enough to just do it on the frontcontroller.
+			 		//Failure to do so can lead to malicious javascript hackers taking control of the youtube media player.
+				    //Example: http://example.com or https://example.com or https://example.com/frontcontroller.php
+                    //If you have https, USE IT! NO EXCUSES!!!
+/**
+     * Constructor
+     *
+     * @param string $youtubeurl the complete URL for a youtube video
+     * @param array $options options for the div.
+     * 
+     * These options are: div-id (string), min-height (int), min-width (int), css-class (string),
+     * resize-able (boolean), and move-able (boolean).
+     * frameborder is also a possible option. But I suggest leaving it as it is already
+     * in the getHTML method near the bottom of this class.
+     *
+     * Do NOT apply a width and height yourself using CSS. The div will resize itself automatically to what options says.
+     * 
+     * You must give the div a ID, which is done trough options. Failure to give a ID when you call method leads to
+     * a die("NO id key defined in $options array, failure to continue execution"); 
+     * 
+     * If you call this class without defining anything (URL and options), a default "nature" movie will be displayed to 
+     * test if resize and moving works.
+     *
+     * See the accompanying README.md for Versions, and more details readin regarding what each method does!
+     *
+     * Code is released as public domain excluding the youtube iframe implentation and any code google owns.
+     * see UNLINCENSE.md, and also read it's exception section so we are clear on what is correct.
+     *
+     */
+    public function __construct($youtubeurl = '', $options = [])
+    {
+    	//If the $youtubeurl is empty, assign a default URL for testing this package and it's full extent of
+    	//resizeable and moveability.
+    	if (empty($youtubeurl)) {
+    		$youtubeurl = "https://www.youtube.com/watch?v=mcixldqDIEQ";
+    		try {
+    		$youtubeid = $this->youtube_id_from_url($youtubeurl);
+    	} catch (Exception $e) 
+    	{print_r('YoutubeURL convertion FAILED For unknown reasons. File a bug report and what<br> you were doing at the time it happened.');}
+    	
+    	}
+		//If the $options array is empty, assign some default values for testing.
+		if (empty($options)) {
+			$options = (object) array(
+            'div-id'        => 'youJStube-Default-ID',
+    		'min-height'	=> '640',
+    		'min-width'		=> '360',
+    		'resize-able'	=> true,
+    		'move-able'		=> true,
+		);
+		}
+        //Check if div-id key is null or if it contains nothing. If it happens. Kill PHP execution.
+        if ($options['div-id'] == null || empty($options['div-id'])) {
+            $Message = <<<EOD
+NO id key defined in $options array in youJStube, failure to continue execution.<br>
+You MUST give $options a div-id key with a string when using the youJStube package.<br>
+Please do so in your code. If you belivie this is a error not from your doing.<br>
+As in you actually gave it a ID. Fill a bug report and describe what you were doing when it happened.
+EOD;
+            die($Message);
+        }
+
+		$this->create($youtubeid, $options);
+    }
+
+    public function create($youtubeid, $options) {
+
+    	$this->youtubeurlID_ = $youtubeid;
+    	$this->options_ = $options;
+
+
+
+    	return $this;
+    }
+
+    public function getHTML() {
+    //All isset checks will be done here. If they are not set. They will be given default values.
+    //==============================================================================================================
+    if (!isset($this->options_['min-width'])) {$this->options_['resize-able'] = 640;}
+    if (!isset($this->options_['min-height'])) {$this->options_['resize-able'] = 360;}    
+    if (!isset($this->options_['resize-able'])) {$this->options_['resize-able'] = true;}
+    if (!isset($this->options_['move-able'])) {$this->options_['move-able'] = true;}
+    //==============================================================================================================
+    //Default setting for frameborder, Change 0 to 1 if you like the frameborder for some wierd reason (it is ugly).
+    $this->options_['frameborder'] = '0';
+    //==============================================================================================================
+    // If checks for width and height to prevent possible errors. Decimals are not okay.
+    //If any of them get's caught in the if checks. It will revent them to default values.
+    if (is_int($this->options_['min-width']) == false && !is_numeric($this->options_['min-width'])) {
+        $this->options_['min-width'] = 640;
+    }
+    if (is_int($this->options_['min-height']) == false && !is_numeric($this->options_['min-height'])) {
+        $this->options_['min-height'] = 360;
+    }
+    
+    
+    //If checks to see if user forgot to set false or true on resize-able and move-able in $options array.
+    //If so, revert to default which is true. 
+    if ($this->options_['resize-able'] == null || empty($this->options_['resize-able'])) {$this->options_['resize-able'] = true;}
+    if ($this->options_['move-able'] == null || empty($this->options_['move-able'])) {$this->options_['move-able']   = true;}
+    //==============================================================================================================
+    $min_width   = $this->options_['min-width'];
+    $min_height  = $this->options_['min-height'];
+
+    if (empty($this->options_['css-class']) || $this->options_['css-class'] == null) {
+        $css_class = '';
+    } else {$css_class   = "class='".$this->options_['css-class']."'";}
+
+    $div_id      = $this->options_['div-id'];
+    if ($this->options_['move-able'] == true) {$move_able = }
+    $resize_able = $this->options_['resize-able'];
+    $move_able   = $this->options_['move-able'];
+
+
+    	$html_jquery = <<<EOD
+<script>
+$('#{$div_id}')
+    .draggable()
+    .resizable();
+</script>
+<div id='{$div_id}' {$css_class} style='width:{$min_width}px; height:{$min_height}px'>
+<iframe id="player" type="text/html" width="{$min_width}" height="{$min_height}"
+src="http://www.youtube.com/embed/{$this->youtubeurlID}?enablejsapi=1&origin={$this->origin_}"
+frameborder="{$this->options_['frameborder']}"></iframe>
+<div>
+
+EOD;
+
+	return $html
+    }
+
+    /**
+ * get youtube video ID from URL
+ * pattern and code borrowed from a stackoverflow post.
+ * Hence it is under Creative Commons. http://creativecommons.org/licenses/by-sa/3.0/
+ *
+ * @param string $url
+ * @return string Youtube video id or FALSE if none found. 
+ */
+function youtube_id_from_url($url) {
+    $pattern = 
+        '%^# Match any youtube URL
+        (?:https?://)?  # Optional scheme. Either http or https
+        (?:www\.)?      # Optional www subdomain
+        (?:             # Group host alternatives
+          youtu\.be/    # Either youtu.be,
+        | youtube\.com  # or youtube.com
+          (?:           # Group path alternatives
+            /embed/     # Either /embed/
+          | /v/         # or /v/
+          | /watch\?v=  # or /watch\?v=
+          )             # End path alternatives.
+        )               # End host alternatives.
+        ([\w-]{10,12})  # Allow 10-12 for 11 char youtube id.
+        $%x'
+        ;
+    $result = preg_match($pattern, $url, $matches);
+    if (false !== $result) {
+        return $matches[1];
+    }
+    return false;
+}
+
+
+
+}
