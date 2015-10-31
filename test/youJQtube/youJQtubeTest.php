2ce0483efca6f31f06e5363d5c027d36328583ef
Christoffer Johansson <christoffer@coldbluegames.com>
2015-10-31 01:24:12 +0100

    Added test changes and .travis.yml

--- test/youJQtube/youJQtubeTest.php
+++ test/youJQtube/youJQtubeTest.php
@@ -78,13 +78,6 @@ class youJQtubeTest extends \PHPUnit_Framework_TestCase
 		);
 
 		$el = new \fynrir\youJQtube\youJQtube($origin, $youtubeurl, $options);
-		
-		$object_vars = get_object_vars($el);
-		
-
-		$this->assertEquals($object_vars['origin_'], $origin, "origin_ in youJQtube object does not match the origin variable in the test!");
-		$this->assertEquals($object_vars['youtubeurlid_'], $expectedyoutubeurlid, "youtubeurlid_ in youJQtube object does not match the expectedyoutubeurlid variable in the test!");
-		$this->assertEquals($object_vars['options_'], $options, "options_ in youJQtube object does not match the options variable in the test!");
 
 	}
 
@@ -110,12 +103,6 @@ class youJQtubeTest extends \PHPUnit_Framework_TestCase
 
 		$el2 = new \fynrir\youJQtube\youJQtube($origin, $youtubeurl, $options);
 		
-		$object_vars = get_object_vars($el2);
-		
-
-		$this->assertEquals($object_vars['origin_'], $origin, "origin_ in youJQtube object does not match the origin variable in the test!");
-		$this->assertEquals($object_vars['youtubeurlid_'], $expectedyoutubeurlid, "youtubeurlid_ in youJQtube object does not match the expectedyoutubeurlid variable in the test!");
-		$this->assertEquals($object_vars['options_'], $options, "options_ in youJQtube object does not match the options variable in the test!");
 
 	}
 	/**
@@ -138,5 +125,154 @@ class youJQtubeTest extends \PHPUnit_Framework_TestCase
 
 	}
 
+	public function testMinWidthCheck() {
+		$origin = "www.example.com";
+		$youtubeurl = "https://www.youtube.com/watch?v=OzAKZiNwzs0";
+		$expectedyoutubeurlid = "OzAKZiNwzs0";
+		$options = array(
+        	'div_id'      => 'youJStube-Default-ID',
+    		'min_height'	=> 400,
+    		'resize_able'	=> true,
+    		'move_able'		=> false,
+		);
+
+		$el = new \fynrir\youJQtube\youJQtube($origin, $youtubeurl, $options);
+		$html = $el->getHTML();
+	}
+
+	public function testMinHeightCheck() {
+		$origin = "www.example.com";
+		$youtubeurl = "https://www.youtube.com/watch?v=OzAKZiNwzs0";
+		$options = array(
+        	'div_id'      => 'youJStube-Default-ID',
+    		'min_width'		=> 400,
+    		'resize_able'	=> true,
+    		'move_able'		=> false,
+		);
+
+		$el = new \fynrir\youJQtube\youJQtube($origin, $youtubeurl, $options);
+		$html = $el->getHTML();
+	}
+
+	public function testCustomCSSClass() {
+		$origin = "www.example.com";
+		$youtubeurl = "https://www.youtube.com/watch?v=OzAKZiNwzs0";
+		$options = array(
+        	'div_id'      => 'youJStube-Default-ID',
+        	'css_class'		=> 'Custom',
+    		'min_width'		=> 400,
+    		'resize_able'	=> true,
+    		'move_able'		=> false,
+    		
+		);
+
+		$el = new \fynrir\youJQtube\youJQtube($origin, $youtubeurl, $options);
+		$html = $el->getHTML();
+	}
+
+	public function testMinWidthFaultyCheck() {
+		$origin = "www.example.com";
+		$youtubeurl = "https://www.youtube.com/watch?v=OzAKZiNwzs0";
+		$options = array(
+        	'div_id'      => 'youJStube-Default-ID',
+        	'css_class'		=> 'Custom',
+    		'min_width'		=> 40.0, //Must be int and contain no decimals. Test will show that my class catches this and makes it default value.
+    		'resize_able'	=> true,
+    		'move_able'		=> false,
+    		
+		);
+
+		$el = new \fynrir\youJQtube\youJQtube($origin, $youtubeurl, $options);
+		$html = $el->getHTML();
+	}
+
+	public function testMinHeightFaultyCheck() {
+		$origin = "www.example.com";
+		$youtubeurl = "https://www.youtube.com/watch?v=OzAKZiNwzs0";
+		$options = array(
+        	'div_id'      => 'youJStube-Default-ID',
+        	'css_class'		=> 'Custom',
+    		'min_height'	=> 40.0, //Must be int and contain no decimals. Test will show that my class catches this and makes it default value.
+    		'resize_able'	=> true,
+    		'move_able'		=> false,
+    		
+		);
+
+		$el = new \fynrir\youJQtube\youJQtube($origin, $youtubeurl, $options);
+		$html = $el->getHTML();
+	}
+
+	public function testMoveAbleContainerCheck() {
+		$origin = "www.example.com";
+		$youtubeurl = "https://www.youtube.com/watch?v=OzAKZiNwzs0";
+		$options = array(
+        	'div_id'      => 'youJStube-Default-ID',
+    		'min_width'		=> 400,
+    		'resize_able'	=> true,
+    		'move_able_container'		=> true,
+		);
+
+		$el = new \fynrir\youJQtube\youJQtube($origin, $youtubeurl, $options);
+		$html = $el->getHTML();
+	}
+
+	public function testMoveAbleContainerYAxisCheck() {
+		$origin = "www.example.com";
+		$youtubeurl = "https://www.youtube.com/watch?v=OzAKZiNwzs0";
+		$options = array(
+        	'div_id'      => 'youJStube-Default-ID',
+    		'min_width'		=> 400,
+    		'resize_able'	=> true,
+    		'move_able_container_y.axis'		=> true,
+		);
+
+		$el = new \fynrir\youJQtube\youJQtube($origin, $youtubeurl, $options);
+		$html = $el->getHTML();
+	}
+
+	public function testMoveAbleContainerXAxisCheck() {
+		$origin = "www.example.com";
+		$youtubeurl = "https://www.youtube.com/watch?v=OzAKZiNwzs0";
+		$options = array(
+        	'div_id'      => 'youJStube-Default-ID',
+    		'min_width'		=> 400,
+    		'resize_able'	=> true,
+    		'move_able_container_x.axis'		=> true,
+		);
+
+		$el = new \fynrir\youJQtube\youJQtube($origin, $youtubeurl, $options);
+		$html = $el->getHTML();
+	}
+
+	public function testResizeAbleContainerCheck() {
+		$origin = "www.example.com";
+		$youtubeurl = "https://www.youtube.com/watch?v=OzAKZiNwzs0";
+		$options = array(
+        	'div_id'      => 'youJStube-Default-ID',
+    		'min_width'		=> 400,
+    		'resize_able_container'	=> true,
+    		'move_able'		=> true,
+		);
+
+		$el = new \fynrir\youJQtube\youJQtube($origin, $youtubeurl, $options);
+		$html = $el->getHTML();
+	}
+
+	public function testYoutubeRegexFalseCheck() {
+		$origin = "www.example.com";
+		$youtubeurl = "https://www.yout47ube.com/watch?v=OzAKZiNwzs0";
+		$options = array(
+        	'div_id'      => 'youJStube-Default-ID',
+    		'min_width'		=> 400,
+    		'resize_able_container'	=> true,
+    		'move_able'		=> true,
+		);
+
+		$el = new \fynrir\youJQtube\youJQtube($origin, $youtubeurl, $options);
+		$html = $el->getHTML();
+	}
+
 
 }
+
+
